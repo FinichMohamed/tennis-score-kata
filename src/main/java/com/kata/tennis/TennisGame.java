2dd204be1f312bf320301db7e8586145edf76731
mohamed FINICH <mohamed.finich@muvraline.fr>
2025-07-28 14:42:04 +0200

    make player A score 15 after one point

--- src/main/java/com/kata/tennis/TennisGame.java
+++ src/main/java/com/kata/tennis/TennisGame.java
@@ -1,8 +1,18 @@
 package com.kata.tennis;
 
 public class TennisGame {
+    private boolean aScored = false;
 
     public String getScore() {
+        if (aScored) {
+            return "Player A : 15 / Player B : 0";
+        }
         return "Player A : 0 / Player B : 0";
     }
+
+    public void pointWonBy(char player) {
+        if (player == 'A') aScored = true;
+    }
+
+
 }
