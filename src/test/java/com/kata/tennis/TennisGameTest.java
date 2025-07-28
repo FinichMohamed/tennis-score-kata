23221d9e2abedb64395f84bf09dc13e2ffb4a03d
mohamed FINICH <mohamed.finich@muvraline.fr>
2025-07-28 13:51:48 +0200

    add initial test for starting score

--- src/test/java/com/kata/tennis/TennisGameTest.java
+++ src/test/java/com/kata/tennis/TennisGameTest.java
@@ -6,7 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TennisGameTest {
 
-
+    @Test
+    void testInitialScore() {
+        TennisGame game = new TennisGame();
+        assertEquals("Player A : 0 / Player B : 0", game.getScore());
+    }
 
 }
 
