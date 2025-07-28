756eab381350f6363ca6f3f3d14240976b1337d4
mohamed FINICH <mohamed.finich@muvraline.fr>
2025-07-28 14:51:04 +0200

    refactor: introduce integer scores for both players

--- src/main/java/com/kata/tennis/TennisGame.java
+++ src/main/java/com/kata/tennis/TennisGame.java
@@ -1,17 +1,29 @@
 package com.kata.tennis;
 
 public class TennisGame {
-    private boolean aScored = false;
+    private int scoreA = 0;
+    private int scoreB = 0;
+
 
     public String getScore() {
-        if (aScored) {
-            return "Player A : 15 / Player B : 0";
+        return "Player A : " + format(scoreA) + " / Player B : " + format(scoreB);
+    }
+    public void pointWonBy(char player) {
+        if (player == 'A') {
+            scoreA++;
+        } else if (player == 'B') {
+            scoreB++;
         }
-        return "Player A : 0 / Player B : 0";
     }
 
-    public void pointWonBy(char player) {
-        if (player == 'A') aScored = true;
+    private String format(int score) {
+        return switch (score) {
+            case 0 -> "0";
+            case 1 -> "15";
+            case 2 -> "30";
+            case 3 -> "40";
+            default -> "40";
+        };
     }
 
 
